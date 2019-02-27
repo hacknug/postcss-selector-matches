5279eb029e33b13cc2f1974e451651715918288a
Nestor Vera <nestorvera@me.com>
2019-02-27 15:15:08 +0100

    Add tests for pseudo-class `:is`

--- test/index.js
+++ test/index.js
@@ -162,3 +162,158 @@ article h3 + p {}`,
 
   t.end()
 })
+
+tape("postcss-selector-is", t => {
+  t.ok(
+    typeof replaceRuleSelector === "function",
+    "expose 'replaceRuleSelector' function (for postcss-custom-selectors)"
+  )
+
+  t.equal(
+    transform("body {}"),
+    "body {}",
+    "should do nothing if there is no :is"
+  )
+
+  t.equal(
+    transform("body, is {}"),
+    "body, is {}",
+    "should really do nothing if there is no :is"
+  )
+
+  t.equal(
+    transform(":is(a, b) {}"),
+    "a, b {}",
+    "should transform simple :is()"
+  )
+
+  t.equal(
+    transform("tag:is(.class, .class2) {}"),
+    "tag.class, tag.class2 {}",
+    "should transform directes :is()"
+  )
+
+  t.equal(
+    transform("tag :is(tag2, tag3) {}"),
+    "tag tag2, tag tag3 {}",
+    "should transform :is()"
+  )
+
+  t.equal(
+    transform("tag :is(tag2, tag3) :is(tag4, tag5) {}"),
+    "tag tag2 tag4, tag tag3 tag4, tag tag2 tag5, tag tag3 tag5 {}",
+    "should transform multiple :is()"
+  )
+
+  t.equal(
+    transform("tag :is(tag2, tag3) :is(tag4, tag5), test {}"),
+    "tag tag2 tag4, tag tag3 tag4, tag tag2 tag5, tag tag3 tag5, test {}",
+    "should transform multiple :is() with stuff after"
+  )
+
+  t.equal(
+    transform(":is(tag) :is(tag2, tag3):hover {}"),
+    "tag tag2:hover, tag tag3:hover {}",
+    "should transform multiple :is() with pseudo after"
+  )
+
+  t.equal(
+    transform("tag :is(tag2 :is(tag4, tag5), tag3) {}"),
+    "tag tag2 tag4, tag tag2 tag5, tag tag3 {}",
+    "should transform :is() recursively"
+  )
+
+  t.equal(
+    transform("p:is(a, span) {}"),
+    "pa, pspan {}",
+    "should transform shit if you ask for shit ?"
+  )
+
+  t.equal(
+    transform(".foo:is(:nth-child(-n+2), .bar) {}"),
+    ".foo:nth-child(-n+2), .foo.bar {}",
+    "should transform childs with parenthesis"
+  )
+
+  t.equal(
+    transform(`a:is(
+  .b,
+  .c
+) {}`),
+    "a.b, a.c {}",
+    "should work with lots of whitespace"
+  )
+
+  t.equal(
+    transform(".foo:is(:nth-child(-n+2), .bar) {}", {lineBreak: true}),
+    ".foo:nth-child(-n+2),\n.foo.bar {}",
+    "should add line break if asked too"
+  )
+
+  t.equal(
+    transform("  .foo:is(:nth-child(-n+2), .bar) {}", {lineBreak: true}),
+    "  .foo:nth-child(-n+2),\n  .foo.bar {}",
+    "should add line break if asked too, and respect indentation"
+  )
+
+  t.equal(
+    transform("\n  .foo:is(:nth-child(-n+2), .bar) {}", {lineBreak: true}),
+    "\n  .foo:nth-child(-n+2),\n  .foo.bar {}",
+    "should add line break if asked too, and respect indentation even with \n"
+  )
+
+  t.equal(
+    transform(`
+button:is(:hover, :active),
+.button:is(:hover, :active) {}`),
+    `
+button:hover, button:active, .button:hover, .button:active {}`,
+    "should avoid duplicates"
+  )
+
+  t.equal(
+    transform(".foo:is(:hover, :focus)::before {}"),
+    ".foo:hover::before, .foo:focus::before {}",
+    "should work with something after :is()"
+  )
+
+  t.equal(
+    transform("article :is(h1, h2, h3) + p {}"),
+    "article h1 + p, article h2 + p, article h3 + p {}",
+    "should work correctly with adjacent selectors"
+  )
+
+  t.equal(
+    transform("article :is(h1, h2, h3) + p {}", {lineBreak: true}),
+    `article h1 + p,
+article h2 + p,
+article h3 + p {}`,
+    "should work correctly with adjacent selectors and line break"
+  )
+
+  t.equal(
+    transform(".foo:is(p) {color: red;}"),
+    "p.foo {color: red;}",
+    "should work correctly with a class and an element"
+  )
+
+  t.equal(
+    transform(".fo--oo > :is(h1, h2, h3) {}"),
+    ".fo--oo > h1, .fo--oo > h2, .fo--oo > h3 {}",
+    "regression https://github.com/postcss/postcss-selector-matches/issues/10"
+  )
+
+  t.equal(
+    transform(":is(h4, h5, h6):hover .ba--z {}"),
+    "h4:hover .ba--z, h5:hover .ba--z, h6:hover .ba--z {}",
+    "regression https://github.com/postcss/postcss-selector-matches/issues/10"
+  )
+
+  t.equal(
+    transform(":is(a, b).foo, .bar {}"),
+    "a.foo, b.foo, .bar {}",
+    "regression https://github.com/postcss/postcss-selector-matches/issues/10"
+  )
+
+  t.end()
+})
